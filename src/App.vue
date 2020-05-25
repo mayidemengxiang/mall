1b30faef2591c5ff5ee12acea664d97a5d0447ec
WLB <wlb@example.com>
2020-05-25 15:24:08 +0800

    合并test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <!--<router-view></router-view>-->
+    <div type="background-color:#e9e9e9;height:49px;display:flex;">
+      <div>首页</div>
+      <div>分类</div>
+      <div>购物车</div>
+      <div>我的</div>
+    </div>
   </div>
 </template>
 
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+
+const route =[
+
+];
+
+const router = new VueRouter({
+  route
+});
+
+export default router;
